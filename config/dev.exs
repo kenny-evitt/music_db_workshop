d599aa9f578dfa65e63c9e79c12bce7f748cd0b1
Kenny Evitt <kenny.evitt@gmail.com>
2019-08-22 10:30:55 -0400

    Configure database credentials
    Issue #1

--- config/dev.exs
+++ config/dev.exs
@@ -1,11 +1,8 @@
 use Mix.Config
 
-{whoami, _} = System.cmd("whoami", [])
-whoami = String.replace(whoami, "\n", "")
-
 config :music_db, MusicDB.Repo,
   database: "music_db_workshop_dev",
-  username: System.get_env("DATABASE_USERNAME") || whoami,
-  password: System.get_env("DATABASE_PASSWORD") || nil,
+  username: "music_db_workshop",
+  password: nil,
   hostname: "localhost"
 
