026e6c60539235420fd60a19a02ec976dfcf7369
Kenny Evitt <kenny.evitt@gmail.com>
2019-08-22 17:59:49 -0400

    Add `port` option for DB to dev config and update test DB config
    Issue #1

--- config/dev.exs
+++ config/dev.exs
@@ -1,8 +1,8 @@
 use Mix.Config
 
 config :music_db, MusicDB.Repo,
+  hostname: "localhost",
+  port: 5433,
   database: "music_db_workshop_dev",
   username: "music_db_workshop",
-  password: nil,
-  hostname: "localhost"
-
+  password: nil
--- config/test.exs
+++ config/test.exs
@@ -1,13 +1,10 @@
 use Mix.Config
 
-{whoami, _} = System.cmd("whoami", [])
-whoami = String.replace(whoami, "\n", "")
-
 config :music_db, MusicDB.Repo,
-  database: "music_db_workshop_test",
-  username: System.get_env("DATABASE_USERNAME") || whoami,
-  password: System.get_env("DATABASE_PASSWORD") || nil,
   hostname: "localhost",
+  port: 5433,
+  database: "music_db_workshop_test",
+  username: "music_db_workshop",
+  password: nil,
   pool: Ecto.Adapters.SQL.Sandbox,
   ownership_timeout: 60_000
-
